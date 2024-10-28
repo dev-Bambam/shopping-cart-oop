b8eedfe27425ddd99db2854c31f76527d8afba07
dev-Bambam <ayogood18@gmail.com>
2024-10-28 12:26:12 +0100

    getting started with the implementations

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-require_once ''
+require_once 'Product.php';
+require_once 'CartItem.php';
+require_once 'Cart.php';
+
+$product = new Product(1, "Samsung", 3500);
+$cartItem = $product->addToCart();
+
+echo "number of item in cart";
+echo Cart::getTotalQuantity();
+echo "total price of items in cart";
+echo Cart::getTotalPrice();
+
+$cartItem = CartItem::increaseQuanity(4);
+echo 
