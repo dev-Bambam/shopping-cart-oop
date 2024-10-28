e4ca6d62afb082947a81f667403c7e854ca34eaa
dev-Bambam <ayogood18@gmail.com>
2024-10-28 15:34:08 +0100

    working on

--- CartItem.php
+++ CartItem.php
@@ -1,4 +1,12 @@
 <?php
 class CartItem{
-    
+    // @param:Product product, quantity;
+    // @methods: increaseQuantity(), decreaseQuantity();
+    public Product $product;
+    public int $quantity;
+
+    public function __construct(Product $product, $quantity){
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
 }
--- Product.php
+++ Product.php
@@ -1,4 +1,19 @@
 <?php
 class Product{
-    
+    /* todo 
+    **@properties: id, price, name
+    */
+    public $id;
+    public $price;
+    public $name;
+    public function __construct($id, $name, $price){
+        $this->name = $name;
+        $this->id = $id;
+        $this->price = $price;
+    }
+    // function add to cart
+    // this function will create a cart item and return it
+    public  function addToCart(){
+
+    }
 }
