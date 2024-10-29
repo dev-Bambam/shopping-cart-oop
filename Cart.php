2454ac58bc83f7fe00aa21cba12f51425e9e60eb
dev-Bambam <ayogood18@gmail.com>
2024-10-29 15:32:26 +0100

    ...

--- Cart.php
+++ Cart.php
@@ -4,6 +4,13 @@ class Cart{
 
     // this function search by id if product exist in the cart and return the id
     public function searchCart(int $id){
-
+        foreach($this->cart as $cartItem){
+            if($cartItem->product->id == $id){
+                return $cartItem->product->id;
+            }
+        }
+    }
+    public function addCartItem(CartItem $cartItem){
+        $this->cart[] = $cartItem;
     }
 }
--- Product.php
+++ Product.php
@@ -13,8 +13,18 @@ class Product{
     }
     // function add to cart
     // this function will create a cart item and return it
-    public  function addToCart(){
-        $cartItem = new CartItem($this, 2);
+    public function addToCart(){
         $cart = new Cart();
+        $cartItem = null;
+
+        // search cart if product exist, if it exist increase the quantity and if not create a new cart item
+        if($cartItem = $cart->searchCart($this->id)){
+            $cart->increaseQuantity($this->id);
+        } else {
+            $cartItem = new CartItem($this, 2);
+            $cart->addCartItem($cartItem);
+        }
+        return $cartItem;
     }
 }
+        
