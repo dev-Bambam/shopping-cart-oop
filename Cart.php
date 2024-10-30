8ef76ba51051404da8fc86aa7edaa16adbfc97fc
dev-Bambam <ayogood18@gmail.com>
2024-10-30 08:07:15 +0100

    pushing the code, and thinking more on the quantity functionality in respect to cartItem creation

--- Cart.php
+++ Cart.php
@@ -1,16 +1,21 @@
 <?php
 class Cart{
+
+    // this array will contain all cart items I am storing using associative array
+    // product id => CartItem
+
     public $cart = [];
 
     // this function search by id if product exist in the cart and return the id
     public function searchCart(int $id){
         foreach($this->cart as $cartItem){
             if($cartItem->product->id == $id){
-                return $cartItem->product->id;
+                return true;
             }
         }
-    }
+    } 
     public function addCartItem(CartItem $cartItem){
-        $this->cart[] = $cartItem;
+        $productId = $cartItem->product->id;
+        $this->cart[$productId] = $cartItem;
     }
 }
--- CartItem.php
+++ CartItem.php
@@ -2,10 +2,10 @@
 class CartItem{
     // @param:Product product, quantity;
     // @methods: increaseQuantity(), decreaseQuantity();
-    public Product $product;
+    public Product $product; 
     public int $quantity;
 
-    public function __construct(Product $product, $quantity){
+    public function __construct(Product $product, $quantity=1){
         $this->product = $product;
         $this->quantity = $quantity;
     } 
